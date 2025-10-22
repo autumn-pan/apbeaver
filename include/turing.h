c6ea870b1046b71d7d769ce971adee5d28b53428
Autumn Pan <pan662607@gmail.com>
2025-10-22 11:03:22 -0400

    [fix]: resolve compile-time errors in turing.c
    fixed instruction halting, added support for more types of turing machines, and switched from using macros to using const

--- include/turing.h
+++ include/turing.h
@@ -4,12 +4,21 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <gmp.h>
 
-#define NUM_STATES 6
+const uint8_t NUM_STATES = 6;
+// This system handles 6 state TMs, so any symbol after 'F' (70) represents a halt
+const uint8_t HLT_SYMBOLS = 71; 
+const uint8_t NUM_SYMBOLS = 2;
+const uint32_t TAPE_LENGTH = UINT32_MAX;
+const uint32_t START_POS = (UINT32_MAX >> 1);
+
 
 enum SIM_RESULT { HALTED, OVERFLOWED, TAPE_EXCEEDED };
 
 typedef struct {
+  bool error; // Whether or not the instruction was successfully parsed
+  bool halting; // Whether or not it is the halt state
   uint8_t write; // What value to write under the head
   bool move;     // false represents left, true represents right
   uint8_t new_state;
@@ -17,8 +26,8 @@ typedef struct {
 
 typedef struct {
   Instruction_t instructions[2][6];
+  mpz_t tape;
   uint32_t head;
   uint8_t state;
 } TuringMachine_t;
-
 #endif
--- src/turing.c
+++ src/turing.c
@@ -3,26 +3,80 @@
 #include <stdio.h>
 #include <string.h>
 
-Instruction_t *parse_instruction(char *str) {
-  Instruction_t *instruction = malloc(sizeof(Instruction_t));
-  if (!instruction) {
-    fprintf(stderr, "Error: Memory allocation failed!");
-    return NULL;
+// Tests will be conducted on BB6 holdout
+// 1RB1LD_1RC0LD_1LD1RE_0LA1LD_0RB0RF_---0RC
+
+// Parse a halt instruction "---"
+bool parse_halt(char *str)
+{
+  if(str[0] != '-' || str[1] != '-' || str[2] != '-')
+    return false;
+
+  str += 3;
+  return true;
+}
+
+Instruction_t parse_instruction(char *str) {
+  Instruction_t instruction;
+  if(parse_halt(str))
+  {
+    instruction.halting = true;
+    return instruction;
   }
 
+  instruction.halting = false;
   if (!isdigit(str[0]))
-    return NULL;
-  instruction->write = atoi(str[0]);
+  {
+    instruction.error = true;
+    return instruction;
+  }
+
+  instruction.write = str[0] - '0';
   str++;
 
-  if (str[0] == "R")
-    instruction->move = true;
-  else if (str[0] == "L")
-    instruction->move = false;
+  if (str[0] == 'R')
+    instruction.move = true;
+  else if (str[0] == 'L')
+    instruction.move = false;
   else
-    return NULL;
-  str++;
+  {
+    instruction.error = true;
+    return instruction;
+  }
 
-  instruction->new_state = str[0] - 'A';
+  str++;
+  instruction.new_state = str[0] - 'A';
+  if(instruction.new_state >= HLT_SYMBOLS)
+    instruction.halting = true;
+  
+  str++;
+  instruction.error = false;
+
   return instruction;
+}
+
+TuringMachine_t *init_turing(char *str) {
+  TuringMachine_t *tm = malloc(sizeof(TuringMachine_t));
+  if (!tm)
+    return NULL;
+
+  // Initialize the tape
+  mpz_init_set_ui(tm->tape, 0);
+  tm->head = START_POS;
+
+  for (int i = 0; i < NUM_STATES; i++) {
+    for (int j = 2; j < NUM_SYMBOLS; j++) {
+      Instruction_t tmp = parse_instruction(str);
+      if(tmp.error)
+        return NULL;
+
+      tm->instructions[i][j] = tmp;
+    }
+
+    // Account for the underscore separating states
+    if(i < NUM_STATES - 1)
+      str++;
+  }
+
+  return tm;
 }
