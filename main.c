0256fa26e49ea94c0680d73e3e49e638a4c904fb
Autumn Pan <pan662607@gmail.com>
2025-10-29 09:00:30 -0400

    [feat]: add command line args

--- main.c
+++ main.c
@@ -1,4 +1,15 @@
-#include "./test/turing_test.h"
 #include "turing.h"
+#include "sims/unacc.h"
+#include <stdio.h>
 
-int main() { test_sim(); }
+int main(int argc, char **argv) 
+{
+    if(argc > 2)
+    {
+        fprintf(stderr, "Error: more than two arguments passed to bb! expected 2.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    TuringMachine_t* tm = init_turing(argv[1]);
+    unaccelerated_sim(tm);
+}
